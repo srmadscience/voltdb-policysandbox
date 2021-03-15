010d021e89c8b4a2e7c5558c59c54035bdaa553f
David Rolfe <drolfe@voltdb.com>
2021-03-15 13:48:01 +0000

    setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu
 . ./.profile
 
-cd voltdb-aggdemo/scripts
+cd voltdb-policydemo/scripts
 
-sqlcmd --servers=vdb1 < ../ddl/voltdb-aggdemo-createDB.sql
+sqlcmd --servers=vdb1 < ../ddl/voltdb-policydemo-createDB.sql
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment topics.xml
