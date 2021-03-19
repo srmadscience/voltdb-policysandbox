5b3426aa906711308704b76b06233e96c799852e
David Rolfe <drolfe@voltdb.com>
2021-03-19 10:34:44 +0000

    Beta

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu
 . ./.profile
 
-cd voltdb-policydemo/scripts
+cd voltdb-policysandbox/scripts
 
 sqlcmd --servers=vdb1 < ../ddl/voltdb-policysandbox-createDB.sql
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment topics.xml
